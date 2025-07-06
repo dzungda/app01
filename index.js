409c8058d5cc9c49a74bca3d8ca78ed01a220faa
root <root@ip-172-31-10-120.ap-northeast-2.compute.internal>
2025-07-06 16:27:13 +0000

    add new content

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Application Chnanges successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes| Hello');
+  res.send('Application Chnanges successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes| Hello new version');
 });
 
 app.listen(port, () => {
