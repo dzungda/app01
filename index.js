2697d67d52cd4e2e099fb6e8f868fa11aa52b793
Dzung Dang <dungdaptit@gmail.com>
2025-07-06 23:13:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Application Chnanges successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes| Hello 0123');
+  res.send('Application Chnanges successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes| Hello');
 });
 
 app.listen(port, () => {
